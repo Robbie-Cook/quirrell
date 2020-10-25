b99848cf5532049e97332a30de0c85c77b3cbe60
Robbie Cook <robbieck7@gmail.com>
2020-10-25 20:09:04 +1300

    Changes

--- packages/quirrell-test/src/App.tsx
+++ packages/quirrell-test/src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React from "react";
+// import { Editor } from "quirrell";
 
 function App() {
   return (
     <main>
+      <h1>Quirrell</h1>
+      <blockquote>A nice text editor</blockquote>
+      <section>
+        <h2>Example</h2>
+        
+      </section>
     </main>
   );
 }
